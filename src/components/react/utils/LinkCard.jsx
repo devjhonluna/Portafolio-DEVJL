c38c7770ffd06f8479a1a7dfeac8da33990007cf
DevJhonLuna <dev.jhonluna@gmail.com>
2024-04-19 16:14:45 -0500

    modificacion del boton de proyectos

--- src/components/react/utils/LinkCard.jsx
+++ src/components/react/utils/LinkCard.jsx
@@ -3,8 +3,8 @@ import { Link, Button } from "@nextui-org/react";
 const LinkCard = ({href1, href2,titulo1,titulo2}) => {
   return (
     <>
-      <Button as={Link} href={href1} color="primary" className="text-primary-100  hover:bg-primary-100 hover:text-default-100 hover:border-primary-100 hover:opacity-100 w-full md:w-fit" isExternal  variant="ghost">{titulo1} </Button>
-      <Button as={Link} href={href2}  color="primary" className="text-primary-100  hover:bg-primary-100 hover:text-default-100 hover:border-primary-100 hover:opacity-100 w-full md:w-fit" isExternal variant="ghost">{titulo2}</Button>
+      <Button as={Link} href={href1} color="default" className="techno-profile__card__btn-ghost hover:!text-background-100 hover:opacity-100 w-full md:w-fit" isExternal  variant="ghost">{titulo1} </Button>
+      <Button as={Link} href={href2}  color="default" className="techno-profile__card__btn-ghost hover:!text-background-100 hover:opacity-100 w-full md:w-fit" isExternal variant="ghost">{titulo2}</Button>
     </>
   );
 };
