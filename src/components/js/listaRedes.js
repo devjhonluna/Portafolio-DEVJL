6217e8ab7e3a44c8158b7a57c88768731c3801aa
DevJhonLuna <dev.jhonluna@gmail.com>
2024-07-05 17:15:44 -0500

    actualizacion del link de tiktok

--- src/components/js/listaRedes.js
+++ src/components/js/listaRedes.js
@@ -3,6 +3,6 @@ import { Github, Linkedin, X,Tiktok } from "@react/icons/Index";
 export let socialMediaList=[
     {id:1,icon:Github,label:"Github",href:"https://github.com/devjhonluna"},
     {id:2,icon:Linkedin,label:"Linkedin",href:"https://www.linkedin.com/in/jhon-alexander-luna-gonzalez-ab30a017a/"},
-    {id:3,icon:Tiktok,label:"Tiktok",href:"https://www.tiktok.com/@dev.jhon.luna?is_from_webapp=1&sender_device=pc"},
+    {id:3,icon:Tiktok,label:"Tiktok",href:"https://x.com/DevJhonluna"},
     {id:4,icon:X,label:"X fka Twitter",href:"https://twitter.com/DevJhonluna"},
 ]
